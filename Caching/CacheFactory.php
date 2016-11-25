578df1a71ed2c50a65ae26fabc82399fee9ebc3b
Benjamin Cremer <b.cremer@shopware.com>
2016-11-25 19:39:34 +0100

    Refactor to use factory instead of events

---
+++ Caching/CacheFactory.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace SwagEssentials\Caching;
+
+use Shopware\Bundle\SearchBundle\ProductSearchInterface;
+use Shopware\Bundle\StoreFrontBundle\Service\ListProductServiceInterface;
+use Shopware\Bundle\StoreFrontBundle\Service\ProductServiceInterface;
+use SwagEssentials\Caching\Caches\CachingListProductDecorator;
+use SwagEssentials\Caching\Caches\CachingProductDecorator;
+use SwagEssentials\Caching\Caches\CachingSearchDecorator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+class CacheFactory
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * @param ListProductServiceInterface $listProductService
+     * @return ListProductServiceInterface
+     */
+    public function createListProductService(ListProductServiceInterface $listProductService)
+    {
+        if (!$this->container->getParameter('swag_essentials.caching_enable_list_product')) {
+            return $listProductService;
+        }
+
+        return new CachingListProductDecorator(
+            $this->container->get('cache'),
+            $listProductService,
+            $this->container->getParameter('swag_essentials.caching_ttl_list_product')
+        );
+    }
+
+    /**
+     * @param ProductServiceInterface $productService
+     * @return ProductServiceInterface
+     */
+    public function createProductService(ProductServiceInterface $productService)
+    {
+        if (!$this->container->getParameter('swag_essentials.caching_enable_product')) {
+            return $productService;
+        }
+
+        return new CachingProductDecorator(
+            $this->container->get('cache'),
+            $productService,
+            $this->container->getParameter('swag_essentials.caching_ttl_product')
+        );
+    }
+
+    /**
+     * @param ProductSearchInterface $productSearch
+     * @return ProductSearchInterface
+     */
+    public function createProductSearch(ProductSearchInterface $productSearch)
+    {
+        if (!$this->container->getParameter('swag_essentials.caching_enable_search')) {
+            return $productSearch;
+        }
+
+        return new CachingSearchDecorator(
+            $this->container->get('cache'),
+            $productSearch,
+            $this->container->getParameter('swag_essentials.caching_ttl_search')
+        );
+    }
+}
--- Caching/Subscriber/Cache.php
+++ Caching/Subscriber/Cache.php
@@ -5,21 +5,15 @@ namespace SwagEssentials\Caching\Subscriber;
 use Enlight\Event\SubscriberInterface;
 use Shopware\Components\Routing\Generators\RewriteGenerator;
 use Shopware\Components\Routing\Router;
-use SwagEssentials\Caching\Caches\CachingListProductDecorator;
-use SwagEssentials\Caching\Caches\CachingProductDecorator;
 use SwagEssentials\Caching\Caches\CachingRewriteGeneratorDecorator;
-use SwagEssentials\Caching\Caches\CachingSearchDecorator;
 
 class Cache implements SubscriberInterface
 {
     public static function getSubscribedEvents()
     {
-        return array(
-            'Enlight_Bootstrap_AfterInitResource_shopware_storefront.list_product_service' => 'onDecorateListProduct',
-            'Enlight_Bootstrap_AfterInitResource_shopware_storefront.product_service' => 'onDecorateProduct',
-            'Enlight_Bootstrap_AfterInitResource_shopware_search.product_search' => 'onDecorateSearch',
+        return [
             'Enlight_Controller_Front_RouteShutdown' => 'onDecorateRouter'
-        );
+        ];
     }
 
     public function onDecorateRouter()
@@ -38,67 +32,15 @@ class Cache implements SubscriberInterface
 
         foreach ($generators as &$generator) {
             if ($generator instanceof RewriteGenerator && !$generator instanceof CachingRewriteGeneratorDecorator) {
-                $generator = new CachingRewriteGeneratorDecorator(Shopware()->Cache(), $generator, Shopware()->Container()->getParameter('swag_essentials.caching_ttl_urls'));
+                $generator = new CachingRewriteGeneratorDecorator(
+                    Shopware()->Cache(),
+                    $generator,
+                    Shopware()->Container()->getParameter('swag_essentials.caching_ttl_urls')
+                );
             }
         }
+        unset($generator);
 
         $router->setGenerators($generators);
-    }
-    
-    public function onDecorateListProduct()
-    {
-        if (!Shopware()->Container()->getParameter('swag_essentials.caching_enable_list_product')) {
-            return;
-        }
-
-        $coreService  = Shopware()->Container()->get('shopware_storefront.list_product_service');
-
-        if ($coreService instanceof CachingListProductDecorator) {
-            return;
-        }
-
-        Shopware()->Container()->set('shopware_storefront.list_product_service', new CachingListProductDecorator(
-            Shopware()->Cache(),
-            $coreService,
-            Shopware()->Container()->getParameter('swag_essentials.caching_ttl_list_product')
-        ));
-    }
-
-    public function onDecorateProduct()
-    {
-        if (!Shopware()->Container()->getParameter('swag_essentials.caching_enable_product')) {
-            return;
-        }
-
-        $coreService  = Shopware()->Container()->get('shopware_storefront.product_service');
-
-        if ($coreService instanceof CachingProductDecorator) {
-            return;
-        }
-
-        Shopware()->Container()->set('shopware_storefront.product_service', new CachingProductDecorator(
-            Shopware()->Cache(),
-            $coreService,
-            Shopware()->Container()->getParameter('swag_essentials.caching_ttl_product')
-        ));
-    }
-
-    public function onDecorateSearch()
-    {
-        if (!Shopware()->Container()->getParameter('swag_essentials.caching_enable_search')) {
-            return;
-        }
-
-        $coreService  = Shopware()->Container()->get('shopware_search.product_search');
-
-        if ($coreService instanceof CachingSearchDecorator) {
-            return;
-        }
-
-        Shopware()->Container()->set('shopware_search.product_search', new CachingSearchDecorator(
-            Shopware()->Cache(),
-            $coreService,
-            Shopware()->Container()->getParameter('swag_essentials.caching_ttl_search')
-        ));
     }
 }
