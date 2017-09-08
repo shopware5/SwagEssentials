9afad8e47b58bc285d7ed58b3dc37bab27cf2ff3
Thomas Eiling <teiling88@gmail.com>
2017-09-08 15:29:31 +0200

    adding docker stack to SwagEssentials

---
+++ _dev-ops/common/actions/init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#DESCRIPTION: execute on app_webserver to provision your environment
+
+rm -rf ./shopware
+./sw.phar install:release -r latest -i ./shopware --db-host __DB_HOST__ --db-user __DB_USER__ --db-password __DB_PASSWORD__ --db-name __DB_NAME__ --shop-host __SW_HOST__
+
+I: mkdir shopware/custom/plugins/SwagEssentials
+
+ln -srf CacheMultiplexer shopware/custom/plugins/SwagEssentials/
+ln -srf Caching shopware/custom/plugins/SwagEssentials/
+ln -srf Common shopware/custom/plugins/SwagEssentials/
+ln -srf PrimaryReplica shopware/custom/plugins/SwagEssentials/
+ln -srf RedisNumberRange shopware/custom/plugins/SwagEssentials/
+ln -srf RedisProductGateway shopware/custom/plugins/SwagEssentials/
+ln -srf RedisStore shopware/custom/plugins/SwagEssentials/
+ln -srf SwagEssentials.php shopware/custom/plugins/SwagEssentials/
