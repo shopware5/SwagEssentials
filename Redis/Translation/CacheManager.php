ac0c9f84380fe63c122f1af7e86c465207d616c3
Thomas Eiling <teiling88@gmail.com>
2017-11-15 21:58:07 +0100

    adding redis translation service

---
+++ Redis/Translation/CacheManager.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types=1);
+
+namespace SwagEssentials\Redis\Translation;
+
+use Shopware\Components\CacheManager as ShopwareCacheManager;
+use Shopware\Components\DependencyInjection\Container;
+use SwagEssentials\Common\CacheManagerDecorationTrait;
+
+/**
+ * Class CacheManager replaces the original CacheManager and collects all caches, which have been invalidated
+ * during a request. Triggers remote cache invalidation on the end of the request
+ */
+class CacheManager extends ShopwareCacheManager
+{
+    use CacheManagerDecorationTrait;
+
+    /**
+     * @var ShopwareCacheManager
+     */
+    private $innerCacheManager;
+
+    /**
+     * @var \Redis
+     */
+    private $redis;
+
+    public function __construct(Container $container, ShopwareCacheManager $innerCacheManager)
+    {
+        parent::__construct($container);
+        $this->innerCacheManager = $innerCacheManager;
+        $this->redis = $container->get('swag_essentials.redis');
+    }
+
+    public function clearConfigCache()
+    {
+        $this->redis->del(Translation::HASH_NAME);
+
+        $this->innerCacheManager->clearConfigCache();
+    }
+}
